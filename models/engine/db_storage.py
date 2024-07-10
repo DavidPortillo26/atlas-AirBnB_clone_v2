715269e29a48b725ae544fdb8a78e1b6c80c7f5f
DavidPortillo26 <anicetod03@gmail.com>
2024-07-10 02:43:26 +0000

    db_storage complete and finished

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -33,3 +33,45 @@ class DBStorage:
             os.environ['HBNB_MYSQL_DB']), pool_pre_ping=True)
         if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query all objects for curent session based on class name"""
+        obj_dict = {}
+        cls = self.all_classes[cls]
+        if cls is not None:
+            objects = self.__session.query(cls).all()
+        else:
+            objects = self.__session.query(
+                State, City, User, Amenity, Place, Review)
+        for obj in objects:
+            key = obj.__class__.__name__ + '.' + obj.id
+            value = obj
+            obj_dict[key] = value
+        return obj_dict
+
+    def new(self, obj):
+        """Add object to current database session"""
+        self.__session.add(obj)
+        self.__session.flush()
+
+    def save(self):
+        """Commit changes to the current databases session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete object from the current database session"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Create tables and current database session"""
+        Base.metadata.create_all(self.__engine)
+
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def close(self):
+        """ call close on private session. """
+        self.__session.close()
